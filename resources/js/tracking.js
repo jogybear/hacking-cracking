03a1c3540a44c935d89910ea0630a79c39f08d75
jogybear <security@hacking-cracking.ch>
2020-12-14 10:55:59 +0100

    Update tracking.js

--- resources/js/tracking.js
+++ resources/js/tracking.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>This is a JavaScript Document Cookie demopage</title>
-		<p style="font-size:30px">document cookie alert</p>
-		<br>
-		<p style="font-size:30px">alert</p>
-	</head>
-	
-	<body>
-		<script>alert(document.cookie)</script>
-	</body>
-	
-</html>
+<script>alert(document.cookie)</script>
